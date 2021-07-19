eb8e004209366b2da914cbfa44810435765feaa0
snandakumar87 <snandaku@redhat.com>
2021-07-18 21:41:53 -0400

    Management ui

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -19,11 +19,15 @@ if (!!window.EventSource) {
 
          if(data.data.amlAlert==='null' && data.data.fraudAlert==='null'){
             row+='<div class="icon"><i class="ni ni-check-bold"></i></td>'
+            row+='<td><button class="btn btn-icon btn-primary" type="button" id="ajaxSubmit" onclick="checkout('+data.data.transactionId+')"><span class="btn-inner--icon">Details<i class="ni ni-bold-right"></i></span></button>';
+
          } else {
             row+='<div class="icon"><i class="ni ni-fat-remove"></i></td>';
+            row+='<td>No Investigation Required!';
+
          }
 
-         row+='<td><button class="btn btn-icon btn-primary" type="button" id="ajaxSubmit" onclick="checkout('+data.data.transactionId+')"><span class="btn-inner--icon">Details<i class="ni ni-bold-right"></i></span></button>';
+
 
          row+='</tr><tr width="200px" id="svg" style="width:100%"></tr>';
 
