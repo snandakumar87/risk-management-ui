f1c1e2475be5163a0f35ba32b9e31456af72f8a8
snandakumar87 <snandaku@redhat.com>
2021-10-05 15:59:12 -0400

    management ui

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -6,16 +6,16 @@ if (!!window.EventSource) {
 
         data = JSON.parse(event.data);
         console.log("data"+JSON.stringify(data));
-        var row = "<td></td>";
-//        var row = '<tr><td>' + data.result.correlationId + '</td><td>' + data.result.subProcessCorrelationId + '</td><td>' ;
-//            row+='<div class="icon"><i class="ni ni-fat-remove"></i></td>';
-//            row+='<td><button class="btn btn-icon btn-primary" type="button" id="ajaxSubmit" onclick="checkout('+data.result.subProcessCorrelationId+')"><span class="btn-inner--icon">Details<i class="ni ni-bold-right"></i></span></button>';
-//
-//
-//
-//
-//
-//         row+='</tr><tr width="200px" id="svg" style="width:100%"></tr>';
+
+        var row = '<tr><td>' + data.correlationId + '</td><td>' + data.subProcessCorrelationId + '</td><td>' + data.entityType + '</td><td>' + data.entityId  ;
+            row+='<div class="icon"><i class="ni ni-fat-remove"></i></td>';
+            row+='<td><button class="btn btn-icon btn-primary" type="button" id="ajaxSubmit" onclick="checkout('+data.result.subProcessCorrelationId+')"><span class="btn-inner--icon">Details<i class="ni ni-bold-right"></i></span></button>';
+
+
+
+
+
+         row+='</tr><tr width="200px" id="svg" style="width:100%"></tr>';
 
         $('#tbody').append(row);
     };
